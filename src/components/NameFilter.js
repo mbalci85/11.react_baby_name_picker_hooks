a45b9e0b7c37630b2d627a48fad5f06b1716aefc
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-02-27 15:19:23 +0000

    added style to filters

--- src/components/NameFilter.js
+++ src/components/NameFilter.js
@@ -3,10 +3,43 @@ import React from 'react';
 const NameFilter = () => {
 	return (
 		<div style={{ textAlign: 'center' }}>
-			<input placeholder="Type to filter names..." />
-			<button>All</button>
-			<button>Boys</button>
-			<button>Girls</button>
+			<input
+				style={{ width: 400, height: 40, marginRight: 15 }}
+				placeholder="Type to filter names..."
+			/>
+			<button
+				style={{
+					border: 'none',
+					margin: 3,
+					padding: 7,
+					borderRadius: 10,
+					backgroundColor: 'lightgray',
+				}}
+			>
+				All
+			</button>
+			<button
+				style={{
+					border: 'none',
+					margin: 3,
+					padding: 7,
+					borderRadius: 10,
+					backgroundColor: 'lightblue',
+				}}
+			>
+				Boys
+			</button>
+			<button
+				style={{
+					border: 'none',
+					margin: 3,
+					padding: 7,
+					borderRadius: 10,
+					backgroundColor: 'lightpink',
+				}}
+			>
+				Girls
+			</button>
 		</div>
 	);
 };
